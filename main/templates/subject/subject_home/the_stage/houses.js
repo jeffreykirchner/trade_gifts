16c5caf1be413ba62b852ae64135cc65c8f4d3de
Jeffrey Kirchner <jkirchner@gmail.com>
2023-07-24 22:22:53 -0700

    dev

--- main/templates/subject/subject_home/the_stage/houses.js
+++ main/templates/subject/subject_home/the_stage/houses.js
@@ -31,14 +31,79 @@ setup_pixi_houses()
         owner_label.eventMode = 'passive'; 
         owner_label.anchor.set(.5, 1);
 
+        //good one        
+        let good_one_sprite = PIXI.Sprite.from(app.pixi_textures[parameter_set_player.good_one+"_tex"]);
+        good_one_sprite.anchor.set(1, 0.5);
+        good_one_sprite.eventMode = 'passive';
+
+        let good_one_label = new PIXI.Text("000", {
+            fontFamily: 'Arial',
+            fontSize: 60,
+            fill: 'white',
+            stroke: 'black',
+            strokeThickness: 2,
+        });
+        good_one_label.eventMode = 'passive'; 
+        good_one_label.anchor.set(0, 0.5);
+
+        //good two        
+        let good_two_sprite = PIXI.Sprite.from(app.pixi_textures[parameter_set_player.good_two+"_tex"]);
+        good_two_sprite.anchor.set(1, 0.5);
+        good_two_sprite.eventMode = 'passive';
+
+        let good_two_label = new PIXI.Text("000", {
+            fontFamily: 'Arial',
+            fontSize: 60,
+            fill: 'white',
+            stroke: 'black',
+            strokeThickness: 2,
+        });
+        good_two_label.eventMode = 'passive'; 
+        good_two_label.anchor.set(0, 0.5);
+
+        //good three        
+        let good_three_sprite = PIXI.Sprite.from(app.pixi_textures[parameter_set_player.good_three+"_tex"]);
+        good_three_sprite.anchor.set(1, 0.5);
+        good_three_sprite.eventMode = 'passive';
+
+        let good_three_label = new PIXI.Text("000", {
+            fontFamily: 'Arial',
+            fontSize: 60,
+            fill: 'white',
+            stroke: 'black',
+            strokeThickness: 2,
+        });
+        good_three_label.eventMode = 'passive'; 
+        good_three_label.anchor.set(0, 0.5);
+
         house_container.addChild(house_sprite);
         house_container.addChild(owner_label);
 
+        house_container.addChild(good_one_sprite);
+        house_container.addChild(good_one_label);
 
+        house_container.addChild(good_two_sprite);
+        house_container.addChild(good_two_label);
+
+        house_container.addChild(good_three_sprite);
+        house_container.addChild(good_three_label);
+        
         owner_label.position.set(0, house_sprite.height/2-2);
+
+        good_one_sprite.position.set(0, -house_sprite.height/4);
+        good_one_label.position.set(0, -house_sprite.height/4);
+
+        good_two_sprite.position.set(0, 0);
+        good_two_label.position.set(0, 0);
+
+        good_three_sprite.position.set(0, house_sprite.height/4);
+        good_three_label.position.set(0, house_sprite.height/4);
 
         pixi_houses[i].house_container = house_container;
         pixi_houses[i].owner_label = owner_label;
+        pixi_houses[i].good_one_label = good_one_label;
+        pixi_houses[i].good_two_label = good_two_label;
+        pixi_houses[i].good_three_label = good_three_label;
 
         pixi_houses[i].house_container.width = app.session.parameter_set.house_width;
         pixi_houses[i].house_container.height = app.session.parameter_set.house_height;
