5c71da6b828a1be681f0df33592f24858a10aedb
Jeffrey Kirchner <jkirchner@gmail.com>
2023-10-02 12:10:36 -0700

    dev

--- main/consumers/staff/session_consumer_mixins/timer.py
+++ main/consumers/staff/session_consumer_mixins/timer.py
@@ -140,7 +140,7 @@ class TimerMixin():
                                                period_number=self.world_state_local["current_period"],
                                                time_remaining=self.world_state_local["time_remaining"],
                                                data={"world_state_local" : self.world_state_local,
-                                                     "world_state_local_avatars" : self.world_state_local,})
+                                                     "world_state_avatars_local" : self.world_state_avatars_local,})
             
             await self.send_message(message_to_self=False, message_to_group=result,
                                     message_type="time", send_to_client=False, send_to_group=True)
--- main/templates/staff/staff_session/replay/replay_card.js
+++ main/templates/staff/staff_session/replay/replay_card.js
@@ -1,15 +1,17 @@
 /**
  * send request to load session events
  */
-send_load_session_events()
+send_load_session_events: function send_load_session_events()
 {
+    app.working = true;
     app.send_message("load_session_events", {});       
+    app.send_load_world_state(1, app.session.parameter_set.period_length-1);
 },
 
 /**
  * take load session events
  */
-take_load_session_events(message_data)
+take_load_session_events: function take_load_session_events(message_data)
 {
     if(message_data.value == "fail")
     {
@@ -18,7 +20,31 @@ take_load_session_events(message_data)
     else
     {
         app.session_events = message_data.session_events;
-        app.session.world_state = message_data.world_state_initial;
-        app.session.world_state["current_experiment_phase"] = "Done";
+        
+        // app.session.world_state = message_data.world_state_initial;
+        // app.session.world_state["current_experiment_phase"] = "Done";
+    }
+},
+
+send_load_world_state: function send_load_world_state(period_number, time_remaining)
+{
+    app.working = true;
+    app.send_message("load_world_state", {period_number : period_number, time_remaining:time_remaining}); 
+},
+
+take_load_world_state: function take_loadd_current_world_state(message_data)
+{
+    if(message_data.value == "fail")
+    {
+          
+    }
+    else
+    {
+        app.session.world_state = message_data.world_state;
+        app.session.world_state_avatars = message_data.world_state_avatars;
+        
+        Vue.nextTick(() => {
+            app.do_reload();                    
+        });
     }
 },
