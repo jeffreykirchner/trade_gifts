f026f926bf24a724cbb319985ad853a4276e440a
Jeffrey Kirchner <jkirchner@gmail.com>
2023-10-16 17:08:41 -0700

    remove transaction from timer update.

--- main/consumers/staff/session_consumer_mixins/timer.py
+++ main/consumers/staff/session_consumer_mixins/timer.py
@@ -67,10 +67,16 @@ class TimerMixin():
         if self.world_state_local["timer_history"][-1]["count"] == math.floor(ts.seconds):
             return
 
-        v = await sync_to_async(sync_continue_timer)(event, self.session_id)
+        v = await sync_to_async(sync_continue_timer)(event, 
+                                                     self.session_id, 
+                                                     self.world_state_local, 
+                                                     self.parameter_set_local)
 
         if v["world_state"]:
+            
             self.world_state_local = v["world_state"]
+            await Session.objects.filter(id=self.session_id).aupdate(world_state=self.world_state_local)
+
             result = {}
 
             #session status
@@ -103,13 +109,6 @@ class TimerMixin():
                 for i in self.world_state_local["patches"]:
                     patch = self.world_state_local["patches"][i]
                     result["patches"][i] = {"levels":patch["levels"], "max_levels":patch["max_levels"]}
-
-                # await SessionEvent.objects.acreate(session_id=self.session_id, 
-                #                                type="timer_tick",
-                #                                period_number=self.world_state_local["current_period"],
-                #                                time_remaining=self.world_state_local["time_remaining"],
-                #                                data={"world_state_local" : self.world_state_local,
-                #                                      "world_state_avatars_local" : self.world_state_avatars_local,})
                    
             #current locations
             result["current_locations"] = {}
@@ -187,149 +186,152 @@ def sync_start_timer(event, session_id):
     
     return {"status" : status, "error_message" : error_message, "world_state" : world_state}
 
-def sync_continue_timer(event, session_id):
+def sync_continue_timer(event, session_id, world_state, parameter_set):
     '''
     timer pulse action
     '''
 
     status = "success"
     error_message = []
-    world_state = None
+    # world_state = None
     earnings = {}
     period_is_over = False
 
-    with transaction.atomic():
-        session = Session.objects.select_for_update().get(id=session_id)
-        current_period = session.get_current_session_period()
-        parameter_set = session.parameter_set.json()
+    # with transaction.atomic():
+    session = Session.objects.get(id=session_id)
+    current_period = session.get_current_session_period()
+    # parameter_set = session.parameter_set.json()
 
-        #check session over
-        if session.world_state["current_period"] >= parameter_set["period_count"] and \
-            session.world_state["time_remaining"] <= 1:
+    #check session over
+    if world_state["current_period"] >= parameter_set["period_count"] and \
+        world_state["time_remaining"] <= 1:
+        
+        # world_state = session.world_state
+
+        world_state["current_period"] = parameter_set["period_count"]
+        world_state["time_remaining"] = 0
+        world_state["timer_running"] = False
+
+        # current_period_id = session.get_current_session_period().id
+
+        #store data
+        for i in world_state["avatars"]:
+            sd_player = current_period.summary_data[i]
             
-            world_state = session.world_state
+            sd_player["end_health"] = world_state["avatars"][i]["health"]
+            sd_player["hat_at_end"] = world_state["avatars"][i]["parameter_set_hat_id"]    
 
-            world_state["current_period"] = parameter_set["period_count"]
-            world_state["time_remaining"] = 0
-            world_state["timer_running"] = False
+            avatar = world_state["avatars"][i]
+            #inventory
+            for k in main.globals.Goods.choices:                       
+                sd_player["house_" + k[0]] = world_state["houses"][str(avatar["parameter_set_player_id"])][k[0]]
+                sd_player["avatar_" + k[0]] = avatar[k[0]]
 
-            current_period_id = session.get_current_session_period().id
+        #session.save()
+        current_period.save()
+        
+        world_state = session.get_current_session_period().do_consumption(world_state, parameter_set)
 
-            #store data
+        world_state["current_experiment_phase"] = ExperimentPhase.NAMES
+        period_is_over = True
+
+        # session.save()
+        
+    if world_state["current_experiment_phase"] != ExperimentPhase.NAMES:
+
+        ts = datetime.now() - datetime.strptime(world_state["timer_history"][-1]["time"],"%Y-%m-%dT%H:%M:%S.%f")
+
+        world_state["timer_history"][-1]["count"] = math.floor(ts.seconds)
+
+        total_time = 0  #total time elapsed
+        for i in world_state["timer_history"]:
+            total_time += i["count"]
+
+        #find current period
+        current_period = 1
+        temp_time = 0          #total of period lengths through current period.
+        for i in range(1, parameter_set["period_count"]+1):
+            temp_time += parameter_set["period_length"]
+
+            #add break times
+            if i % parameter_set["break_frequency"] == 0:
+                temp_time += parameter_set["break_length"]
+            
+            if temp_time > total_time:
+                break
+            else:
+                current_period += 1
+
+        #time remaining in period
+        time_remaining = temp_time - total_time
+
+        # if current_period == 2 and time_remaining ==10:
+        #     '''test code'''
+        #     pass
+
+        world_state["time_remaining"] = time_remaining
+        world_state["current_period"] = current_period
+        session.save()
+
+        period_is_over = False
+        if current_period > 1:
+            last_period_id = world_state["session_periods_order"][current_period - 2]
+            last_period = world_state["session_periods"][str(last_period_id)]
+
+            period_is_over = not last_period["consumption_completed"]
+
+        #check if period over
+        if period_is_over:
+            
+            last_period = session.session_periods.get(id=last_period_id)
+            summary_data_last = last_period.summary_data
+
+            #store data for last period
             for i in world_state["avatars"]:
-                sd_player = current_period.summary_data[i]
-                
-                sd_player["end_health"] = world_state["avatars"][i]["health"]
-                sd_player["hat_at_end"] = world_state["avatars"][i]["parameter_set_hat_id"]    
+                sd_player = summary_data_last[i]
 
-                avatar = world_state["avatars"][i]
+                #health at end
+                sd_player["end_health"] = world_state["avatars"][i]["health"]        
+
+                #hat
+                sd_player["hat_at_end"] = world_state["avatars"][i]["parameter_set_hat_id"]          
+                
                 #inventory
+                avatar = world_state["avatars"][i]
                 for k in main.globals.Goods.choices:                       
                     sd_player["house_" + k[0]] = world_state["houses"][str(avatar["parameter_set_player_id"])][k[0]]
-                    sd_player["avatar_" + k[0]] = avatar[k[0]]
+                    sd_player["avatar_" + k[0]] =  avatar[k[0]]
 
-            session.save()
-            current_period.save()
+            last_period.save()
+
+            world_state = last_period.do_consumption(world_state, parameter_set)
+            world_state = session.get_current_session_period().do_timer_actions(time_remaining, world_state, parameter_set)
+            world_state = session.get_current_session_period().do_production(world_state, parameter_set)
+            world_state = session.get_current_session_period().do_patch_growth(world_state, parameter_set)
+
+            current_session_period = session.get_current_session_period()
+            summary_data_current = current_session_period.summary_data
             
-            session = session.get_current_session_period().do_consumption()
-            session.world_state["current_experiment_phase"] = ExperimentPhase.NAMES
-            period_is_over = True
+            for i in world_state["avatars"]:
+                sd_player = summary_data_current[i]
+                # session.world_state["session_players"][i]["earnings"] += session.world_state["session_players"][i]["inventory"][current_period_id]
 
-            session.save()
-            
-        if session.world_state["current_experiment_phase"] != ExperimentPhase.NAMES:
-
-            ts = datetime.now() - datetime.strptime(session.world_state["timer_history"][-1]["time"],"%Y-%m-%dT%H:%M:%S.%f")
-
-            session.world_state["timer_history"][-1]["count"] = math.floor(ts.seconds)
-
-            total_time = 0  #total time elapsed
-            for i in session.world_state["timer_history"]:
-                total_time += i["count"]
-
-            #find current period
-            current_period = 1
-            temp_time = 0          #total of period lengths through current period.
-            for i in range(1, parameter_set["period_count"]+1):
-                temp_time += parameter_set["period_length"]
-
-                #add break times
-                if i % parameter_set["break_frequency"] == 0:
-                    temp_time += parameter_set["break_length"]
+                # earnings[i] = {}
+                # earnings[i]["total_earnings"] = 0
+                # earnings[i]["period_earnings"] = 0
                 
-                if temp_time > total_time:
-                    break
-                else:
-                    current_period += 1
+                #store starting health
+                sd_player["start_health"] = world_state["avatars"][i]["health"]
+                
+            current_session_period.save()
+            # session.save()
 
-            #time remaining in period
-            time_remaining = temp_time - total_time
-
-            # if current_period == 2 and time_remaining ==10:
-            #     '''test code'''
-            #     pass
-
-            session.world_state["time_remaining"] = time_remaining
-            session.world_state["current_period"] = current_period
-            session.save()
-
-            period_is_over = False
-            if current_period > 1:
-                last_period_id = session.world_state["session_periods_order"][current_period - 2]
-                last_period = session.world_state["session_periods"][str(last_period_id)]
-
-                period_is_over = not last_period["consumption_completed"]
-
-            #check if period over
-            if period_is_over:
-               
-                world_state = session.world_state
-                last_period = session.session_periods.get(id=last_period_id)
-                summary_data_last = last_period.summary_data
-
-                #store data for last period
-                for i in world_state["avatars"]:
-                    sd_player = summary_data_last[i]
-
-                    #health at end
-                    sd_player["end_health"] = world_state["avatars"][i]["health"]        
-
-                    #hat
-                    sd_player["hat_at_end"] = world_state["avatars"][i]["parameter_set_hat_id"]          
-                    
-                    #inventory
-                    avatar = world_state["avatars"][i]
-                    for k in main.globals.Goods.choices:                       
-                        sd_player["house_" + k[0]] = world_state["houses"][str(avatar["parameter_set_player_id"])][k[0]]
-                        sd_player["avatar_" + k[0]] =  avatar[k[0]]
-
-                last_period.save()
-
-                session = last_period.do_consumption()
-                session = session.get_current_session_period().do_timer_actions(time_remaining)
-                session = session.get_current_session_period().do_production()
-                session = session.get_current_session_period().do_patch_growth()
-
-                current_session_period = session.get_current_session_period()
-                summary_data_current = current_session_period.summary_data
-               
-                for i in world_state["avatars"]:
-                    sd_player = summary_data_current[i]
-                    # session.world_state["session_players"][i]["earnings"] += session.world_state["session_players"][i]["inventory"][current_period_id]
-
-                    # earnings[i] = {}
-                    # earnings[i]["total_earnings"] = 0
-                    # earnings[i]["period_earnings"] = 0
-                    
-                    #store starting health
-                    sd_player["start_health"] = world_state["avatars"][i]["health"]
-                   
-                current_session_period.save()
-                session.save()
-
-            else:
-                session = session.get_current_session_period().do_timer_actions(time_remaining)
+        else:
+            world_state = session.get_current_session_period().do_timer_actions(time_remaining, world_state, parameter_set)
           
-        world_state = session.world_state
+        # world_state = session.world_state
 
-    return {"status" : status, "error_message" : error_message, "world_state" : world_state, "period_is_over" : period_is_over}
+    return {"status" : status, 
+            "error_message" : error_message, 
+            "world_state" : world_state, 
+            "period_is_over" : period_is_over}
