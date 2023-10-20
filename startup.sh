4d47331a4d9a119e5445ebae30ee4c4eab1b6b5a
Jeffrey Kirchner <jkirchner@gmail.com>
2023-10-20 13:10:28 -0700

    update startup

--- startup.sh
+++ startup.sh
@@ -7,6 +7,6 @@ echo "Install redis"
 curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 apt-get update
-apt-get install redis
+apt-get -y install redis
 echo "Start Daphne:"
 daphne -b 0.0.0.0 _trade_gifts.asgi:application
