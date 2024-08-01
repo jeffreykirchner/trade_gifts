844cc813533ce4aab3797b10c43e9afdf1e9872a
Jeffrey Kirchner <jkirchner@gmail.com>
2024-08-01 15:54:28 -0700

    update tests.

--- main/tests/__init__.py
+++ main/tests/__init__.py
@@ -6,3 +6,4 @@ from .test_subject_consumer import  *
 from .test_production import  *
 from .test_consumption import  *
 from .test_subject_consumer_patch import  *
+from .test_subject_consumer_group_gate import  *
