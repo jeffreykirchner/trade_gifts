21a89cd1aad21e566e39c95276a4c00d61a2c51b
Jeffrey Kirchner <jkirchner@gmail.com>
2024-08-26 13:49:59 -0700

    Refactor chat text processing and display logic

--- main/templates/subject/subject_home/chat/chat_card.js
+++ main/templates/subject/subject_home/chat/chat_card.js
@@ -2,8 +2,33 @@ send_chat: function send_chat(){
 
     if(app.working) return;
     if(app.chat_text.trim() == "") return;
-    if(app.chat_text.trim().length > 200) return;
-    if(app.session.world_state.avatars[app.session_player.id].sleeping) return;
+    if(app.chat_text.trim().length > 100) return;
+
+    let chat_bubble_sprite = PIXI.Sprite.from(app.pixi_textures.sprite_sheet_2.textures["chat_bubble.png"]);
+    let style = new PIXI.TextStyle({fontFamily : 'Arial',
+                                    fontSize: 18, 
+                                    fill :0x000000, 
+                                    align : 'left',
+                                    wordWrap: true,
+                                    wordWrapWidth: chat_bubble_sprite.width-25,
+                                    breakWords: true})
+    const textMetrics = PIXI.CanvasTextMetrics.measureText(app.chat_text.trim(), style)
+
+    let chat_text_processed ="";
+
+    for(let i=0; i<textMetrics.lines.length; i++)
+    {
+        chat_text_processed += textMetrics.lines[i];
+
+        if(i==5 || i==textMetrics.lines.length-1) break;
+
+        chat_text_processed += "\n";
+    }
+
+    if(textMetrics.lines.length > 6)
+    {
+        chat_text_processed += "...";
+    }
     
     if(app.session.world_state.current_experiment_phase == 'Instructions')
     {
@@ -13,7 +38,7 @@ send_chat: function send_chat(){
     {
         app.working = true;
         app.send_message("chat", 
-                        {"text" : app.chat_text.trim(),
+                        {"text" : chat_text_processed,
                         "current_location" : app.session.world_state_avatars.session_players[app.session_player.id].current_location,},
                         "group");
     }
